487ace46575e6831b58dbdc8c0028c97baac8471
Chris Holtz <choltz3@mail.greenriver.edu>
2019-04-26 16:33:04 -0400

    Added validation to order function

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+session_start();
+
 
 require_once('vendor/autoload.php');
 
@@ -11,7 +13,12 @@ $f3 = Base::instance();
 
 $f3->set('colors', array('pink', 'green', 'blue'));
 
+require_once('model/validation-functions.php');
+
 $f3->route('GET|POST /', function (){
+
+    session_destroy();
+
     echo "<h1>My Pets</h1>";
     echo "<a href='order'>Order a Pet</a>";
 
@@ -34,6 +41,20 @@ $f3->route('GET /@animal', function($f3, $params)
 });
 
 $f3->route("GET|POST /order", function($f3){
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        if(validString($_POST['name']))
+        {
+            $_SESSION['animal'] = $_POST['name'];
+            $f3->reroute("/order2");
+        }
+        else
+        {
+            $f3->set("errors['animal']", "Please enter an animal");
+        }
+    }
+
     $view = new Template();
     echo $view->render("views/views.html");
 });
